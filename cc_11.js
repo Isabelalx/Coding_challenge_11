4b2ce8839728bee1416e5ca324a9b5593ded84ae
Isabelalx <isabelalopesxavier@usf.edu>
2025-03-02 17:16:10 -0500

    Task 3 - Created Library Class

--- cc_11.js
+++ cc_11.js
@@ -55,3 +55,28 @@ console.log(borrower1.borrowedBooks);
 borrower1.returnBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks);
 // Expected output: []
+
+// Task 3
+// Creating a library class
+
+class Library {
+    constructor(books, borrowers) {
+        this.books = []; // Creating properties 
+        this.borrowers = [];
+    }
+
+    addBook(book) {
+        this.books.push(book);
+        return this.books;
+    }
+
+    listBooks() {
+        this.books.forEach(book => console.log(book.getDetails()));
+    }
+}
+
+// Test Cases task 3
+const library = new Library();
+library.addBook(book1);
+library.listBooks();
+// Expected output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
